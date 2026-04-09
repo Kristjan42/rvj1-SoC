a98daa1613dda9bedff54d819a3dfc2357b172e7
“Kristjan42” <kristjan.brcina@outlook.com>
2026-04-09 22:07:05 +0200

    Modified crossbar testing module to be able to test new obi crossbar slave module implementation

--- soc/obi-crossbar/rtl/obi_xbar_testing_module.sv
+++ soc/obi-crossbar/rtl/obi_xbar_testing_module.sv
@@ -9,7 +9,7 @@ module obi_xbar_testing_module #(
     parameter int MANAGERS = 2,
     parameter int MID_WIDTH = $clog2(MANAGERS),
     parameter int SUBORDINATES = 8,
-    parameter int ID_WIDTH = 32
+    parameter int ID_WIDTH = 4
 )
 (
     input   logic clk_i,
@@ -48,6 +48,7 @@ module obi_xbar_testing_module #(
     output  logic [ID_WIDTH-1:0]    lsu_rsp_id_o,
     input   logic                   lsu_rsp_ready_i,
 
+/*
 // OBI RAM A port signals
     // request channel
     output   logic [ADDR_WIDTH-1:0]  obi_00_aaddr_o,
@@ -101,11 +102,26 @@ module obi_xbar_testing_module #(
     input  logic [ID_WIDTH-1:0]    obi_11_rid_i,
     input  logic                   obi_11_rvalid_i,
     output logic                   obi_11_rready_o
-    
+    */
+
+// S0
+    output logic [ADDR_WIDTH-1:0]           s0_obi_aadr_o,
+    output logic                            s0_obi_awe_o,
+    output logic [NBytes-1:0]               s0_obi_abe_o,
+    output logic [DATA_WIDTH-1:0]           s0_obi_awdata_o,
+    output logic                            s0_obi_areq_o,
+    input  logic                            s0_obi_agnt_i,
+
+    output logic                            s0_obi_rready_o,
+    input  logic                            s0_obi_rvalid_i,
+    input  logic [DATA_WIDTH-1:0]           s0_obi_rdata_i,
+    input  logic                            s0_obi_rerr_i
+
 );
 
 parameter int NBytes = DATA_WIDTH / 8;
 
+    /*
     // OBI UART
     //soc_defines::obi_manager_2_sub  uart_a_obii;
     soc_defines::obi_a uart_a_obii;
@@ -173,46 +189,60 @@ parameter int NBytes = DATA_WIDTH / 8;
     assign obi_10_mid_o = ramb_a_obii.obi_mid;
     logic  ramb_agnt_obii;
     assign ramb_agnt_obii = obi_10_agnt_i;
-    
+    */
     
 
     obi_crossbar #(
         32,
         32,
         2,
-        3,
+        1,
         4
     ) xbar (
         .clk_i(clk_i),
         .rstn_i(rstn_i),
         // IFU
-        .ifu_req_addr_i(ifu_req_addr_i),
-        .ifu_req_data_i(ifu_req_data_i),
-        .ifu_req_strobe_i(ifu_req_strobe_i),
-        .ifu_req_write_i(ifu_req_write_i),
-        .ifu_req_valid_i(ifu_req_valid_i),
-        .ifu_req_ready_o(ifu_req_ready_o),
+        .m0_req_addr_i(ifu_req_addr_i),
+        .m0_req_data_i(ifu_req_data_i),
+        .m0_req_strobe_i(ifu_req_strobe_i),
+        .m0_req_write_i(ifu_req_write_i),
+        .m0_req_valid_i(ifu_req_valid_i),
+        .m0_req_ready_o(ifu_req_ready_o),
 
-        .ifu_rsp_data_o(ifu_rsp_data_o),
-        .ifu_rsp_error_o(ifu_rsp_error_o),
-        .ifu_rsp_valid_o(ifu_rsp_valid_o),
-        .ifu_rsp_ready_i(ifu_rsp_ready_i),
-        .ifu_rsp_id_o(ifu_rsp_id_o),
+        .m0_rsp_data_o(ifu_rsp_data_o),
+        .m0_rsp_error_o(ifu_rsp_error_o),
+        .m0_rsp_valid_o(ifu_rsp_valid_o),
+        .m0_rsp_ready_i(ifu_rsp_ready_i),
+        .m0_rsp_id_o(ifu_rsp_id_o),
 
         // LSU
-        .lsu_req_addr_i(lsu_req_addr_i),
-        .lsu_req_data_i(lsu_req_data_i),
-        .lsu_req_strobe_i(lsu_req_strobe_i),
-        .lsu_req_write_i(lsu_req_write_i),
-        .lsu_req_ready_o(lsu_req_ready_o),
-        .lsu_req_valid_i(lsu_req_valid_i),
+        .m1_req_addr_i(lsu_req_addr_i),
+        .m1_req_data_i(lsu_req_data_i),
+        .m1_req_strobe_i(lsu_req_strobe_i),
+        .m1_req_write_i(lsu_req_write_i),
+        .m1_req_ready_o(lsu_req_ready_o),
+        .m1_req_valid_i(lsu_req_valid_i),
         
-        .lsu_rsp_data_o(lsu_rsp_data_o),
-        .lsu_rsp_error_o(lsu_rsp_error_o),
-        .lsu_rsp_ready_i(lsu_rsp_ready_i),
-        .lsu_rsp_id_o(lsu_rsp_id_o),
-        .lsu_rsp_valid_o(lsu_rsp_valid_o),
+        .m1_rsp_data_o(lsu_rsp_data_o),
+        .m1_rsp_error_o(lsu_rsp_error_o),
+        .m1_rsp_ready_i(lsu_rsp_ready_i),
+        .m1_rsp_id_o(lsu_rsp_id_o),
+        .m1_rsp_valid_o(lsu_rsp_valid_o),
 
+        // S0
+        .s0_obi_aadr_o(s0_obi_aadr_o),
+        .s0_obi_awe_o(s0_obi_awe_o),
+        .s0_obi_abe_o(s0_obi_abe_o),
+        .s0_obi_awdata_o(s0_obi_awdata_o),
+        .s0_req_valid_o(s0_obi_areq_o),
+        .s0_req_read_i(s0_obi_agnt_i),
+
+        .s0_rsp_ready_o(s0_obi_rready_o),
+        .s0_rsp_write_i(s0_obi_rvalid_i),
+        .s0_obi_rdata_i(s0_obi_rdata_i),
+        .s0_obi_rerr_i(s0_obi_rerr_i)
+
+        /*
         // UART
         .uart_r_obio_i(uart_r_obio),
         .uart_rready_obii_o(uart_rready_obio),
@@ -230,6 +260,7 @@ parameter int NBytes = DATA_WIDTH / 8;
         .ramb_rready_obii_o(ramb_rready_obio),
         .ramb_a_obii_o(ramb_a_obii),
         .ramb_agnt_obio_i(ramb_agnt_obii)
+        */
 
     );
 
